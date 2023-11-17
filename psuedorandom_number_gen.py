9a74d0d04398d06752b6220fdf436fd83d2a51f8
Jonty <17radcj@olab.org.uk>
2023-11-17 17:32:50 +0000

    Auto stash before merge of "main" and "origin/main"

--- psuedorandom_number_gen.py
+++ psuedorandom_number_gen.py
@@ -13,6 +13,7 @@ def key_scheduling(key):
     return s
 
 def psuedoRandom_Gen(s, repeats):
+    nums = []
     i = 0
     j = 0
     for x in range(repeats):
@@ -22,11 +23,12 @@ def psuedoRandom_Gen(s, repeats):
         s[i] = s[j]
         s[j] = temp
         PSRNumber = s[(s[i]+s[j]) % 256]
-        print(PSRNumber)
+        nums.append(PSRNumber)
+    return nums
 
 def get_psuedo_random_nums(key, r):
     s = key_scheduling(key)
-    psuedoRandom_Gen(s, r)
+    return psuedoRandom_Gen(s, r)
 
 def get_ciphertext(plaintext, key):
     plaintext = list(plaintext)
@@ -34,14 +36,18 @@ def get_ciphertext(plaintext, key):
     count = -1
     for letter in plaintext:
         count +=1
-        new_letter = chr(int(bin(ord(letter))^bin(key[count])))
-        ciphertext.append(new_letter)
+        letter = ord(letter)
+        new_letter = letter^key[count]
+        #ciphertext.append(hex(int(new_letter)))
+        ciphertext.append(chr(int(new_letter)))
+    return ciphertext
          
 
 
 key = "AQACS"
 r = 256
-get_psuedo_random_nums(key, r)
+plaintext = "Computer Science"
 
-get_ciphertext("Compu", [241,99,212,73,127])
+key = get_psuedo_random_nums(key, r)
+print(get_ciphertext(plaintext, key))
     
